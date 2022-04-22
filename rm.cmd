603cef870ac93678bb1546eed1e022166f76ed65
KOSON <18818209416@139.com>
2022-04-22 11:22:15 +0800

    TinyGo

---
+++ rm.cmd
@@ -0,0 +1,20 @@
+rm adapter_s*
+rm adapter_nrf*
+rm adapter_win*
+rm adapter_dar*
+rm gap_nrf*
+rm gap_win*
+rm gap_dar*
+rm gattc_s*
+rm gattc_d*
+rm gatts_s*
+rm gatts_o*
+rm -r .git
+rm -r .circleci
+rm -r s1*
+rm  *.md
+rm  *_sd.go
+rm  *_darwin.go
+rm  LICENSE
+rm -r winbt 
+rm -r images
